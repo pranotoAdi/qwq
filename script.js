7da8d5db40c3924df0cce8b2393178ae32bbe192
pranotoAdi <38913715+pranotoAdi@users.noreply.github.com>
2021-02-17 23:01:49 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
+
+
+async function getQuote() {
+    const proxyUrl = 'https://yeshop.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText;
+    } catch(error){
+        getQuote()      
+    }
+}
+
+getQuote();
